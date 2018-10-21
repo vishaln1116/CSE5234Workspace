b9f7be65674451635b29a56a41be12f01a46c293
Vishal Nallanagulagari <nallanagulagari.4@osu.edu>
2018-10-21 17:07:11 -0400

    Setup h2 database and connected to app

--- MyRestaurant-InventoryManagement-EJB/ejbModule/edu/osu/cse5234/business/InventoryServiceBean.java
+++ MyRestaurant-InventoryManagement-EJB/ejbModule/edu/osu/cse5234/business/InventoryServiceBean.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Session Bean implementation class InventoryServiceBean
@@ -19,54 +21,61 @@ public class InventoryServiceBean implements InventoryService {
 
 	Inventory currentInventory;
 	
-    /**
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public static final String MY_QUERY = "Select i from Item i"; 
+
+	/**
      * Default constructor. 
      */
     public InventoryServiceBean() {
-        Item[] items = new Item[5];
-
-        items[0] = new Item();
-        items[0].setName("Pumpkin pie");
-        items[0].setPrice("5.00");
-
-        items[1] = new Item();
-        items[1].setName("Apple pie");
-        items[1].setPrice("4.00");
-
-        items[2] = new Item();
-        items[2].setName("Double chocolate chunk cake");
-        items[2].setPrice("7.00");
-
-        items[3] = new Item();
-        items[3].setName("Deluxe granola bar");
-        items[3].setPrice("10.00");
-
-        items[4] = new Item();
-        items[4].setName("Hardtack");
-        items[4].setPrice("0.20");
-
-        Inventory inv = new Inventory();
-        
-        ArrayList<Item> lst = new ArrayList<>();
-
-        for (Item i : items) {
-        	i.setQuantity("5");
-            lst.add(i);
-        }
-        
-        inv.setItems(lst);
-        
-		currentInventory = inv;
+    	currentInventory = new Inventory();
+//        Item[] items = new Item[5];
+//
+//        items[0] = new Item();
+//        items[0].setName("Pumpkin pie");
+//        items[0].setPrice("5.00");
+//
+//        items[1] = new Item();
+//        items[1].setName("Apple pie");
+//        items[1].setPrice("4.00");
+//
+//        items[2] = new Item();
+//        items[2].setName("Double chocolate chunk cake");
+//        items[2].setPrice("7.00");
+//
+//        items[3] = new Item();
+//        items[3].setName("Deluxe granola bar");
+//        items[3].setPrice("10.00");
+//
+//        items[4] = new Item();
+//        items[4].setName("Hardtack");
+//        items[4].setPrice("0.20");
+//
+//        Inventory inv = new Inventory();
+//        
+//        ArrayList<Item> lst = new ArrayList<>();
+//
+//        for (Item i : items) {
+//        	i.setQuantity("5");
+//            lst.add(i);
+//        }
+//        
+//        inv.setItems(lst);
+//        
+//		currentInventory = inv;
     }
 
 	@Override
 	public Inventory getAvailableInventory() {
+		List<Item> items = entityManager.createQuery(MY_QUERY, Item.class).getResultList();
+		currentInventory.setItems(items);
 		return currentInventory;
 	}
 
 	@Override
 	public boolean validateQuantity(List<Item> items) {
-		//assume true, then check otherwise
 		boolean valid = true;
 		for (int i = 0; i < items.size() && valid; i++) {
 			valid = Integer.parseInt(currentInventory.itemByName(items.get(i).getName()).getQuantity()) 
@@ -83,6 +92,14 @@ public class InventoryServiceBean implements InventoryService {
 			currentInventory.itemByName(item.getName()).setQuantity("" + newAmount);
 		}
 		return true;
+	}
+	
+    public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
 	}
 
 }
--- MyRestaurant-InventoryManagement-EJBClient/ejbModule/edu/osu/cse5234/business/view/Item.java
+++ MyRestaurant-InventoryManagement-EJBClient/ejbModule/edu/osu/cse5234/business/view/Item.java
@@ -1,6 +1,15 @@
 package edu.osu.cse5234.business.view;
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ITEM")
 public class Item implements Serializable {
 
     /**
@@ -8,16 +17,58 @@ public class Item implements Serializable {
 	 */
 	private static final long serialVersionUID = 6205737053442725966L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	
+	@Column(name="ITEM_NUMBER")
+	private int itemNumber;
+	
+	@Column(name="DESCRIPTION")
+	private String description;
+	
+	@Column(name="NAME")
 	private String name;
 
+	@Column(name="UNIT_PRICE")
     private String price;
 
+	@Column(name="AVAILABLE_QUANTITY")
     private String quantity;
 
     public Item() {
+    	this.id = -1;
+    	this.itemNumber = -1;
+    	this.description = "";
         this.name = "";
         this.price = "0";
+        this.quantity = "0";
     }
+    
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getItemNumber() {
+		return itemNumber;
+	}
+
+	public void setItemNumber(int itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
     public String getName() {
         return this.name;

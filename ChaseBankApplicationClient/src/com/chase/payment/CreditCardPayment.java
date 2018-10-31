5511d3fe159eac768c618c78e38efffdd3ea780b
Michael Trunk <39310930+trunk-student@users.noreply.github.com>
2018-10-31 19:24:11 -0400

    Added WebService configuration - Michael

---
+++ ChaseBankApplicationClient/src/com/chase/payment/CreditCardPayment.java
@@ -0,0 +1,168 @@
+
+package com.chase.payment;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for creditCardPayment complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="creditCardPayment">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="ccHolderName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="ccNumber" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="cvvCode" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="expirationDate" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="paymentAmount" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "creditCardPayment", propOrder = {
+    "ccHolderName",
+    "ccNumber",
+    "cvvCode",
+    "expirationDate",
+    "paymentAmount"
+})
+public class CreditCardPayment {
+
+    protected String ccHolderName;
+    protected String ccNumber;
+    protected String cvvCode;
+    protected String expirationDate;
+    protected Double paymentAmount;
+
+    /**
+     * Gets the value of the ccHolderName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCcHolderName() {
+        return ccHolderName;
+    }
+
+    /**
+     * Sets the value of the ccHolderName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCcHolderName(String value) {
+        this.ccHolderName = value;
+    }
+
+    /**
+     * Gets the value of the ccNumber property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    /**
+     * Sets the value of the ccNumber property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCcNumber(String value) {
+        this.ccNumber = value;
+    }
+
+    /**
+     * Gets the value of the cvvCode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCvvCode() {
+        return cvvCode;
+    }
+
+    /**
+     * Sets the value of the cvvCode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCvvCode(String value) {
+        this.cvvCode = value;
+    }
+
+    /**
+     * Gets the value of the expirationDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    /**
+     * Sets the value of the expirationDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setExpirationDate(String value) {
+        this.expirationDate = value;
+    }
+
+    /**
+     * Gets the value of the paymentAmount property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    /**
+     * Sets the value of the paymentAmount property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setPaymentAmount(Double value) {
+        this.paymentAmount = value;
+    }
+
+}
